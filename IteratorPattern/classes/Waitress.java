fac7d703e3287c82f9cf37f8207721cceff83a2f
n0aHVN <superherodung123@gmail.com>
2025-07-16 10:59:13 +0700

    Create Waitress.java

---
+++ IteratorPattern/classes/Waitress.java
@@ -0,0 +1,15 @@
+package IteratorPattern.classes;
+
+public class Waitress {
+    public void printMenu(){}
+    public void printBreakfastMenu(){}
+    public void printLunchMenu() {
+        
+    }
+    public void printVegetarianMenu(){
+
+    }
+    public void isItemVegetarian(){
+        
+    }
+}
