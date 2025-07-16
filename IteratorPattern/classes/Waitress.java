d5b8541a1adbf461d225dacb5be8f8ffb93baac2
n0aHVN <superherodung123@gmail.com>
2025-07-16 22:02:00 +0700

    Update Waitress.java

--- IteratorPattern/classes/Waitress.java
+++ IteratorPattern/classes/Waitress.java
@@ -1,10 +1,30 @@
 package IteratorPattern.classes;
 
+import IteratorPattern.iterators.Iterator;
+
 public class Waitress {
-    public void printMenu(){}
-    public void printBreakfastMenu(){}
-    public void printLunchMenu() {
-        
+    PancakeHouseMenu pancakeHouseMenu;
+    DinnerMenu dinnerMenu;
+    public Waitress(PancakeHouseMenu pancakeHouseMenu, DinnerMenu dinnerMenu){
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinnerMenu = dinnerMenu;
+    }
+    public void printMenu(){
+        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
+        Iterator dinnerIterator = dinnerMenu.createIterator();
+
+        System.out.println("MENU BREAKFAST");
+        printMenu(pancakeIterator);
+        System.out.println("MENU LUNCH");
+        printMenu(dinnerIterator);
+    }
+    public void printMenu(Iterator iterator){
+        while(iterator.hasNext()){
+            MenuItem item = iterator.next();
+            System.out.print(item.getName()+ ", ");
+            System.out.print(item.getName()+ " -- ");
+            System.out.println(item.getDescription());
+        }
     }
     public void printVegetarianMenu(){
 
